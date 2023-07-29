5be3658484e0e731853674785cd23afef513678d
Avinash Nair <avinair108@gmail.com>
2023-07-29 17:24:12 -0500

    Refactoring

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,7 @@
 import React from 'react'
 import { animateScroll as scroll } from 'react-scroll'
-import {
-    FooterContainer,
-    FooterWrap,
-    // FooterLinksContainer,
-    // FooterLinksWrapper,
-    // FooterLinkTitle,
-    // FooterLinkItems,
-    // FooterLink,
-    SocialMedia,
-    SocialMediaWrap,
-    SocialLogo
-} from './FooterElements'
+import './FooterElements.css';
+import { Link} from 'react-router-dom';
 
 const Footer = () => {
 
@@ -19,55 +9,17 @@ const toggleHome = () => {
     scroll.scrollToTop();
 }
   return (
-    <FooterContainer>
-        <FooterWrap>
-            {/* <FooterLinksContainer>
-                <FooterLinksWrapper>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>Thank You</FooterLinkTitle>
-                            <FooterLink to="/">For Visiting</FooterLink>
-                            <FooterLink to="/">My Portfolio</FooterLink>
-                            <FooterLink to="/">I Made This</FooterLink>
-                            <FooterLink to="/">Footer For Fun</FooterLink>
-                            <FooterLink to="/">And Aesthetics</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>You Can</FooterLinkTitle>
-                            <FooterLink to="/">Click These</FooterLink>
-                            <FooterLink to="/">Links To</FooterLink>
-                            <FooterLink to="/">See If One</FooterLink>
-                            <FooterLink to="/">Contains A Secret</FooterLink>
-                            <FooterLink to="/">Surprise</FooterLink>
-                    </FooterLinkItems>
-                </FooterLinksWrapper>
-                <FooterLinksWrapper>
-                <FooterLinkItems>
-                        <FooterLinkTitle>Thank You</FooterLinkTitle>
-                            <FooterLink to="/">For Visiting</FooterLink>
-                            <FooterLink to="/">My Portfolio</FooterLink>
-                            <FooterLink to="/">I Made This</FooterLink>
-                            <FooterLink to="/">Footer For Fun</FooterLink>
-                            <FooterLink to="/">And Aesthetics</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>You Can</FooterLinkTitle>
-                            <FooterLink to="/">Click These</FooterLink>
-                            <FooterLink to="/">Links To</FooterLink>
-                            <FooterLink to="/">See If One</FooterLink>
-                            <FooterLink to="/">Contains A Secret</FooterLink>
-                            <FooterLink to="/">Surprise</FooterLink>
-                    </FooterLinkItems>
-                </FooterLinksWrapper>
-            </FooterLinksContainer> */}
-            <SocialMedia>
-            <SocialMediaWrap>
-                <SocialLogo to='/' onClick={toggleHome}>
+    <footer className='footerContainer'>
+        <div className='footerWrap'>
+            <section className='socialMedia'>
+            <div className='socialMediaWrap'>
+                <Link className='logo' to='/' onClick={toggleHome}>
                 Avi Nair {new Date().getFullYear()}
-                </SocialLogo>
-            </SocialMediaWrap>
-            </SocialMedia>
-        </FooterWrap>
-    </FooterContainer>
+                </Link>
+            </div>
+            </section>
+        </div>
+    </footer>
   )
 }
 
