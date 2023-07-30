4cfe494278d4ea112901c8238a043cf8e0063cbc
Avinash Nair <avinair108@gmail.com>
2023-07-30 10:51:45 -0500

    Major fixes

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,24 +1,25 @@
-import React, {useState} from 'react'
-import Video from '../../video/video.mp4'
-import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper,
-ArrowForward, ArrowRight } from './HeroElements'
-import { Button } from '../ButtonElement'
+import React from 'react'
+// import Video from '../../video/video.mp4'
+// import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper,
+// ArrowForward, ArrowRight } from './HeroElements'
+import {HeroBg } from './HeroElements'
+// import { Button } from '../ButtonElement'
 import Typewriter from 'typewriter-effect';
 import './LandingPage.css'
 import Image from '../../images/react-image.jpg'
 
 const HeroSection = () => {
-  const [hover, setHover] = useState(false);
+  // const [hover, setHover] = useState(false);
 
-  const onHover = () =>{
-    setHover(!hover);
-  };
+  // const onHover = () =>{
+  //   setHover(!hover);
+  // };
 
   return (
     <div className='mainContainer' id='home'>
         <HeroBg>
             {/* <VideoBg autoPlay loop muted src={Video} type='video/mp4' /> */}
-            <img className='mainImage' src={Image}/>
+            <img className='mainImage' src={Image} alt='inspirational'/>
         </HeroBg>
         <div className='mainContent'> 
             <h1 className='mainHeader'>
