be265b29dd4f6a0de56d741668e0e27fddf6de04
Avinash Nair <avinair108@gmail.com>
2023-07-30 12:03:08 -0500

    Refactored

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,26 +1,14 @@
 import React from 'react'
-// import Video from '../../video/video.mp4'
-// import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper,
-// ArrowForward, ArrowRight } from './HeroElements'
-import {HeroBg } from './HeroElements'
-// import { Button } from '../ButtonElement'
 import Typewriter from 'typewriter-effect';
 import './LandingPage.css'
 import Image from '../../images/react-image.jpg'
 
 const HeroSection = () => {
-  // const [hover, setHover] = useState(false);
-
-  // const onHover = () =>{
-  //   setHover(!hover);
-  // };
-
   return (
     <div className='mainContainer' id='home'>
-        <HeroBg>
-            {/* <VideoBg autoPlay loop muted src={Video} type='video/mp4' /> */}
+        <div className='mainBackground'>
             <img className='mainImage' src={Image} alt='inspirational'/>
-        </HeroBg>
+        </div>
         <div className='mainContent'> 
             <h1 className='mainHeader'>
             <Typewriter
@@ -33,26 +21,6 @@ const HeroSection = () => {
             }}
           />
             </h1>
-          {/* <p className='mainText'>
-            Welcome
-          </p> */}
-          {/* <div className='mainButtonWrapper'>
-            <Button 
-            to='about' 
-            onMouseEnter={onHover} 
-            onMouseLeave={onHover}
-            primary='true'
-            dark='true'
-            smooth={true}
-            duration={500}
-            spy={true}
-            exact='true'
-            offset={-80}
-            >
-              About Me   
-            {hover ? <ArrowForward /> : <ArrowRight />}
-            </Button>
-          </div> */}
         </div>
     </div>
   )
